0beaaae20fcbfdb853bd929c59f3403f6d46aea9
haz314 <haz314@users.noreply.github.com>
2018-06-28 19:47:05 -0400

    pull coinmarketcap data

---
+++ assets/javascript/coinmarket.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+    //CoinMarketCap Query URL
+    var queryURL = "https://api.coinmarketcap.com/v2/listings/";
+
+    //AJAX CoinMarketCap
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        .then(function(response) {
+
+            console.log(response);
+
+            var results = JSON.stringify(response.data);
+            console.log(results);
+
+            $("#testDiv01").append(response);
+
+        });
+});
