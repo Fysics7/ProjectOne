ede3f110578f5188e4064eb11e46145d0fbb0aee
haz314 <haz314@users.noreply.github.com>
2018-06-30 10:14:07 -0400

    Pull crypto data in desc MC w/ images

--- assets/javascript/coinmarket.js
+++ assets/javascript/coinmarket.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     
-    //CoinMarketCap Query URL
+    /*//CoinMarketCap Query URL
     var queryURL = "https://api.coinmarketcap.com/v2/listings/";
 
     //AJAX CoinMarketCap
@@ -8,13 +8,12 @@ $(document).ready(function() {
         url: queryURL,
         method: "GET"
     })
-
         .then(function(response) {
 
-            console.log(response);
+            //console.log(response);
 
             var results = response.data;
-            console.log(results);
+            //console.log(results);
 
             $("#testDiv01").append(results);
 
@@ -28,5 +27,37 @@ $(document).ready(function() {
                 $("#testDiv01").append(holder);
 
             }
-        });
+        });*/
+
+    
+    var queryURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+
+        console.log(response);
+
+        var results = response;
+
+        $("#testDiv01").append(results);
+
+        for (i = 0; i < results.length; i++) {
+
+            var name = results[i].name;
+            var id = results[i].id;
+            var symbol = results[i].symbol;
+            var image = results[i].image;
+            var currentPrice = results[i].current_price
+
+            var holder = $("<div>").text(id + " " + symbol + " " + name + " " + currentPrice);
+
+            var imgHolder = $("<img src=\"" + image + "\">");
+
+            $("#testDiv01").append(imgHolder);
+            $("#testDiv01").append(holder);
+        }
+    });
 });
